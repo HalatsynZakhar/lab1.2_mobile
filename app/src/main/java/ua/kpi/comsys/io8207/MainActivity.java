d7d6fd399ba52576363db16344d96d08bb3e5155
Галацин Захар <54953439+HalatsynZakhar@users.noreply.github.com>
2021-04-03 01:13:42 +0300

    Update MainActivity.java

--- app/src/main/java/ua/kpi/comsys/io8207/MainActivity.java
+++ app/src/main/java/ua/kpi/comsys/io8207/MainActivity.java
@@ -1,3 +1,40 @@
+package ua.kpi.comsys.io8207;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        lab12();
+    }
+    
     public void lab12() {
         System.out.println(" \n\nLab1.2 Start-------------------------------------------------\n\n ");
 
